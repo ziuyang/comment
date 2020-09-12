3e092b10156f3cc33717ca7bb2d41d194a5a4efd
김지우 <youyexindejw@gmail.com>
2020-09-12 13:48:27 +0900

    댓글 삭제 기능 추가

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('update/<int:pk>/', views.update, name='update'),
     path('delete/<int:pk>/', views.delete, name='delete'),
     path('comment/update/<int:pk>', views.comment_update, name='comment_update'),
+    path('comment/delete/<int:pk>', views.comment_delete, name='comment_delete'),
 ]
